f82b56c7f91d09d2eae67d3902f1c3e52bb5a29b
Jacob Conacher <jake.conacher@gmail.com>
2022-10-04 13:32:41 -0500

    Copied over Book app to have the correct App name

--- Books/BooksApp.swift
+++ Books/BooksApp.swift
@@ -2,19 +2,16 @@
 //  BooksApp.swift
 //  Books
 //
-//  Created by Madi Lumsden on 10/4/22.
+//  Created by Jacob Conacher on 10/1/22.
 //
 
 import SwiftUI
 
 @main
 struct BooksApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
--- Books/ContentView.swift
+++ Books/ContentView.swift
@@ -2,87 +2,74 @@
 //  ContentView.swift
 //  Books
 //
-//  Created by Madi Lumsden on 10/4/22.
+//  Created by Jacob Conacher on 10/1/22.
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        ScrollView(showsIndicators: false) {
+            VStack(spacing: 20) {
+                Image("cover")
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(10)
+                    .frame(width: 200, height: 250)
+                    .shadow(color: .black, radius: 6, x: 5, y: 5)
+                
+                VStack(spacing: 1) {
+                    Text("Neil Smyth")
+                        .font(.system(size: 12))
+                        .opacity(0.5)
+                    
+                    Text("SwiftUI Essentials")
+                        .font(.system(size: 20, weight: .bold))
+                    
+                    Text("iOS 16 Edition")
+                        .font(.system(size: 15, weight: .bold))
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                
+                Text("""
+                        This book will teach you the skills necessary to build iOS 16 applications using SwiftUI, Xcode 14, and the Swift 5.7 programming language.\n\nBeginning with the basics, this book outlines the steps necessary to set up an iOS development environment, together with an introduction to using Swift Playgrounds to learn and experiment with Swift.\n\nThe book also includes in-depth chapters introducing the Swift 5.7 programming language, including data types, control flow, functions, object-oriented programming, property wrappers, structured concurrency, and error handling.\n\nA guided tour of Xcode in SwiftUI development mode follows an introduction to the key concepts of SwiftUI and project architecture. The book also covers creating custom SwiftUI views and explains how these views are combined to create user interface layouts, including stacks, frames, and forms.\n\nOther topics covered include data handling using state properties and observable, state, and environment objects, as are key user interface design concepts such as modifiers, lists, tabbed views, context menus, user interface navigation, and outline groups.\n\nThe book also includes chapters covering graphics drawing, user interface animation, view transitions and gesture handling, WidgetKit, document-based apps, Core Data, CloudKit, and SiriKit integration.\n\nChapters also explain how to integrate SwiftUI views into existing UIKit-based projects and integrate UIKit code into SwiftUI.\n\nFinally, the book explains how to package up a completed app and upload it to the App Store for publication.\n\nAlong the way, the topics covered in the book are put into practice through detailed tutorials, the source code for which is also available for download.\n\nThe aim of this book, therefore, is to teach you the skills necessary to build your own apps for iOS 16 using SwiftUI. Assuming you are ready to download the iOS 16 SDK and Xcode 14 and have an Apple Mac system, you are ready to get started.\n\nBeginning with the basics, this book provides an outline of the steps necessary to set up an iOS development environment together with an introduction to the use of Swift Playgrounds to learn and experiment with Swift.
+                        """)
+                    .font(.system(size: 10))
+                    .opacity(0.5)
+                    .frame(width: 300)
+                
+                HStack(spacing: 30) {
+                    Text("Programming")
+                        .font(.system(size: 14))
+                        .padding(5)
+                        .overlay(RoundedRectangle(cornerRadius: 100)
+                        .stroke(.black, lineWidth: 2))
+                    
+                    Text("SwiftUI")
+                        .font(.system(size: 14))
+                        .padding(5)
+                        .overlay(RoundedRectangle(cornerRadius: 100)
+                        .stroke(.black, lineWidth: 2))
+                    
+                    Text("Print")
+                        .font(.system(size: 14))
+                        .padding(5)
+                        .overlay(RoundedRectangle(cornerRadius: 100)
+                        .stroke(.black, lineWidth: 2))
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                
+                Text("Buy for $39.99")
+                    .font(.system(size: 14))
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .background(RoundedRectangle(cornerRadius: 30).fill(.black))
+                
             }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
